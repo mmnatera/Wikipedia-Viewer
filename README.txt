1ca28af2ce671d58e2a40ee04a0a6a0752f6413c
Maria Natera <mmnatera@gmail.com>
2018-03-07 00:36:31 -0600

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,4 @@
+A Pen created at CodePen.io. You can find this one at https://codepen.io/mmnatera/pen/RQQqox.
+
+ FreeCodeCamp Project.
+Requirements: I can search Wikipedia entries in a search box and see the resulting Wikipedia entries, I can click a button to see a random Wikipedia entry,  experiment with Wikipedia's API.
---
+++ js/index.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+  $("#getResult").click(function() {
+    // clearing the content of the list
+    $("ul").empty();
+    var search = $("#search").val();
+    $.ajax({
+      type: 'GET',
+      url: 'https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=' + search,
+      dataType: 'jsonp',
+      success: function(result) {
+        console.log(result);
+        var obj = result.query.pages;
+        for (var prop in obj) {
+          $("ul").append('<a target="_blank" href="https://en.wikipedia.org/wiki?curid=' + obj[prop].pageid + '">' + '<li><h3 class="title ">' + obj[prop].title + '</h3><p class="text">' + obj[prop].extract + '</p></li></a>');
+        }
+      }
+    });
+  });
+
+  // starting a search after enter is       //pressed
+  $("#search").keypress(function(e) {
+    if (e.which == 13) {
+      $("#getResult").click();
+    }
+  });
+
+  // removing the outline of the buttons
+  // when clicked
+  $('#getResult, #randomResult').click(function() {
+    $(this).blur();
+  });
+});
